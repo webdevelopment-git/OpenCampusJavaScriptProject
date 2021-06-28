bb78854482009dbcd5f7ea90a26998c331c3c390
webdevelopment-git <Contact me via GitHub instead>
2021-06-28 02:59:00 +0200

    initialiseBoard() added

--- JS/interactivity.js
+++ JS/interactivity.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     keepValuesUpdated();
+    addControllListeners();
 });
 
 let frequency = 5.0;
@@ -7,6 +8,8 @@ let probability = 0.5;
 let boardHeight = 3;
 let boardWidth = 3;
 let cellSize = 32;
+
+let boardStates = [];
 
 function keepValuesUpdated() {
     document.getElementById("frequencySlider").addEventListener("change", function(){
@@ -20,26 +23,32 @@ function keepValuesUpdated() {
     document.getElementById("probabilitySlider").addEventListener("change", function(){
         probability = document.getElementById("probabilitySlider").value;
         document.getElementById("probabilityInput").value = probability;
+        initialiseBoard();
     });
     document.getElementById("probabilityInput").addEventListener("change", function(){
         probability = document.getElementById("probabilityInput").value;
         document.getElementById("probabilitySlider").value = probability;
+        initialiseBoard();
     });
     document.getElementById("boardHeightSlider").addEventListener("change", function(){
         boardHeight = document.getElementById("boardHeightSlider").value;
         document.getElementById("boardHeightInput").value = boardHeight;
+        initialiseBoard();
     });
     document.getElementById("boardHeightInput").addEventListener("change", function(){
         boardHeight = document.getElementById("boardHeightInput").value;
         document.getElementById("boardHeightSlider").value = boardHeight;
+        initialiseBoard();
     });
     document.getElementById("boardWidthSlider").addEventListener("change", function(){
         boardWidth = document.getElementById("boardWidthSlider").value;
         document.getElementById("boardWidthInput").value = boardWidth;
+        initialiseBoard();
     });
     document.getElementById("boardWidthInput").addEventListener("change", function(){
         boardWidth = document.getElementById("boardWidthInput").value;
         document.getElementById("boardWidthSlider").value = boardWidth;
+        initialiseBoard();
     });
     document.getElementById("cellSizeSlider").addEventListener("change", function(){
         cellSize = document.getElementById("cellSizeSlider").value;
@@ -61,11 +70,53 @@ function keepValuesUpdated() {
     });
 }
 
-/*
-button startRun
-button stopRun
-button resetRun
-button stepForward
-button toggleRun
-button stepBackward
-*/
+function addControllListeners() {
+    document.getElementById("startRun").addEventListener("click", function() {
+        console.log("Start run");
+        initialiseBoard();
+    });
+    document.getElementById("stopRun").addEventListener("click", function() {
+        console.log("Stop run");
+    });
+    document.getElementById("resetRun").addEventListener("click", function() {
+        console.log("Reset run");
+        initialiseBoard();
+    });
+    document.getElementById("stepForward").addEventListener("click", function() {
+        console.log("One step forward");
+    });
+    document.getElementById("stepBackward").addEventListener("click", function() {
+        console.log("One step backward");
+    });
+}
+
+function initialiseBoard() {
+    boardStates[0] = [];
+    for (let row = 0; row < boardHeight; row++) {
+        let rowCells = [];
+        for (let column = 0; column < boardWidth; column++) {
+            let randomNum = Math.floor(Math.random()*100)+1;
+            rowCells[column] = (randomNum <= probability*100);
+        }
+        boardStates[0][row] = rowCells;
+    }
+    document.getElementById("board").innerHTML="";
+    for (let row = 0; row < boardHeight; row++) {
+        let rowDiv = document.createElement("div");
+        rowDiv.className = "row";
+        rowDiv.id = "row"+row;
+        for (let column = 0; column < boardWidth; column++) {
+            let cell = document.createElement("div");
+            cell.className="cell";
+            cell.id=row + "|" + column;
+            if (boardStates[0][row][column]) {
+                cell.style.backgroundColor = "#00AA00";
+            } else {
+                cell.style.backgroundColor = "#000000";
+            }
+            rowDiv.appendChild(cell);
+        }
+        document.getElementById("board").appendChild(rowDiv);
+    }
+    console.log(boardStates);
+}
